869784dd975402caffb35fbecab6a9da675f1e36
kroussen <kroussen.322@gmail.com>
2023-08-06 16:57:59 +0600

    Added routes and models

--- app.py
+++ app.py
@@ -1,27 +1,49 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, Task
+import os
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.getcwd(), 'instance/tasks.db')
+db.init_app(app)
 
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def all_tasks():
-    return 'Все задачи'
+    tasks = Task.query.all()
+    return render_template('all_tasks.html', tasks=tasks)
 
 
 @app.route('/new_task', methods=('GET', 'POST'))
 def new_task():
-    return 'новая задвча'
+    if request.method == 'POST':
+        task = request.form.get('task')
+        new_task = Task(title=task)
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect(url_for('all_tasks'))
+    return render_template('new_task.html')
 
 
 @app.route('/delete_task/<int:task_id>', methods=['POST'])
 def delete_task(task_id):
-    return 'удалить задачу'
+    task = Task.query.get(task_id)
+    if task:
+        db.session.delete(task)
+        db.session.commit()
+    return redirect(url_for('all_tasks'))
 
 
 @app.route('/complete_task/<int:task_id>', methods=['POST'])
 def complete_task(task_id):
-    return 'завершить задачу'
+    task = Task.query.get(task_id)
+    if task:
+        task.completed = True
+        db.session.commit()
+    return redirect(url_for('all_tasks'))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
